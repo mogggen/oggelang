48dd6530a84357e0f6a51bf1e56c30408b34c87f
Magnus Lind <sungam999@gmail.com>
2020-10-26 23:40:46 +0100

    compile done?

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,5 +1,6 @@
 #include "compiler.h"
 #include <unordered_map>
+#include <algorithm>
 #include "error.h"
 
 constexpr unsigned long hash_djb2(const char *str)
@@ -15,10 +16,13 @@ constexpr unsigned long hash_djb2(const char *str)
 
 struct CompileCtx
 {
+    struct AddrLinenum{unsigned long long addr; int line_num;};
     std::vector<int> program_data;
+    std::vector<AddrLinenum> program_line_num;
     std::vector<int> static_data;
     std::unordered_map<unsigned long, int> var_table;
     std::vector<int> var_addr;
+    std::vector<AddrLinenum> goto_addr;
 };
 
 int get_variable(std::unordered_map<unsigned long, int>& var_table, const char* name, FileLocation loc)
@@ -110,12 +114,16 @@ int compile_statement(CompileCtx& cc, AstStatement* stmt)
     {
         case StatementType::GOTO:
             {
-
+                cc.program_line_num.push_back({cc.program_data.size(), stmt->loc.line});
+                cc.program_data.push_back((int)OpCode::GOTO);
+                cc.goto_addr.push_back({cc.program_data.size(), stmt->goto_line_number});
+                cc.program_data.push_back(0);
             } break;
         case StatementType::IF:
             {
                 if(compile_expression(cc, stmt->expression))
                 {
+                    cc.program_line_num.push_back({cc.program_data.size(), stmt->loc.line});
                     cc.program_data.push_back((int)OpCode::IF);
                     int location = cc.program_data.size();
                     cc.program_data.push_back(0);
@@ -139,6 +147,7 @@ int compile_statement(CompileCtx& cc, AstStatement* stmt)
             {
                 if(compile_expression(cc, stmt->expression))
                 {
+                    cc.program_line_num.push_back({cc.program_data.size(), stmt->loc.line});
                     if(stmt->print_as_char)
                         cc.program_data.push_back((int)OpCode::PRINTC);
                     else
@@ -153,6 +162,7 @@ int compile_statement(CompileCtx& cc, AstStatement* stmt)
                 int addr = get_variable(cc.var_table, stmt->var_name, stmt->loc);
                 if(addr > 0)
                 {
+                    cc.program_line_num.push_back({cc.program_data.size(), stmt->loc.line});
                     cc.program_data.push_back((int)OpCode::MOVED);
                     cc.program_data.push_back(addr);
                     cc.var_addr.push_back(cc.program_data.size()-1);
@@ -174,9 +184,44 @@ void compile(AstStatement* root)
         statement = statement->next;
     }
 
+    for(auto g : cc.goto_addr)
+    {
+        auto it = cc.program_line_num.end();
+        while(it != cc.program_line_num.begin()) // can use a binary search here.
+        {
+            --it;
+            if(g.line_num <= it->line_num)
+                cc.program_data[g.addr] = it->addr;
+        }
+    }
+
+    for(auto a : cc.var_addr)
+        cc.program_data[a] += cc.program_data.size();
+
     printf("\nprogram data:\n");
-    for(int i : cc.program_data)
-        printf("%d\n", i);
+    int j = 0;
+    for(int i = 0; i < cc.program_data.size() ; i++)
+    {
+        printf("%d", cc.program_data[i]);
+        if( j < cc.program_line_num.size() && i == cc.program_line_num[j].addr )
+            printf(" %d\n", cc.program_line_num[j++].line_num);
+        else
+        {
+            bool found = false;
+            for(auto s : cc.goto_addr)
+            {
+                if(s.addr == i)
+                {
+                    printf(" goto %d\n", s.line_num);
+                    found = true;
+                    break;
+                }
+            }
+            if(!found)
+                printf("\n");
+        }
+    }
+
     
     printf("\nstatic data:\n");
     for(int i : cc.static_data)
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,9 +1,29 @@
 #include "interpreter.h"
-#include "parser.h"
+
+#include <vector>
 
 void report_runtime_errro(const char* msg)
 {
     printf("RUNTIME ERROR: %s\n", msg);
+}
+
+struct Stack
+{
+    int sp;
+    int* stack;
+};
+
+inline void push(Stack& stack, int value)
+{
+    stack.stack[++stack.sp] = value;
+}
+inline int PEEK(Stack& stack)
+{
+    return stack.stack[stack.sp];
+}
+inline int POP(Stack& stack)
+{
+    return stack.stack[stack.sp--];
 }
 
 void run(ByteCode code)
@@ -13,6 +33,10 @@ void run(ByteCode code)
 
     std::vector<int> heap;
 
+    Stack stack;
+    stack.sp = 0;
+    stack.stack = (int*)malloc(sizeof(int)*1024);
+
     bool flag_equal = false;
     bool flag_greater = false;
 
@@ -20,220 +44,81 @@ void run(ByteCode code)
     {
          switch((OpCode)code.data[pc++])
         {
+            case OpCode::PUSHC: 
+                {
+                } break;
+            case OpCode::PUSHV:
+                {
+                } break;
+            case OpCode::POP:
+                {
+                } break;
+
             case OpCode::ADD: 
                 {
-                    int v = mem[mem[pc+1]];
-                    mem[mem[pc]] += v;
-                    pc += 2;
                 } break;
             case OpCode::SUB:
                 {
-                    int v = mem[mem[pc+1]];
-                    mem[mem[pc]] -= v;
-                    pc += 2;
                 } break;
             case OpCode::MULT:
                 {
-                    int v = mem[mem[pc+1]];
-                    mem[mem[pc]] *= v;
-                    pc += 2;
                 } break;
             case OpCode::DIV:
                 {
-                    int v = mem[mem[pc+1]];
-                    mem[mem[pc]] /= v;
-                    pc += 2;
                 } break;
             case OpCode::AND:
                 {
-                    int v = mem[mem[pc+1]];
-                    mem[mem[pc]] &= v;
-                    pc += 2;
                 } break;
             case OpCode::OR:
                 {
-                    int v = mem[mem[pc+1]];
-                    mem[mem[pc]] |= v;
-                    pc += 2;
                 } break;
             case OpCode::XOR:
                 {
-                    int v = mem[mem[pc+1]];
-                    mem[mem[pc]] ^= v;
-                    pc += 2;
                 } break;
             case OpCode::CMP:
                 {
-                    int v = mem[mem[pc+1]];
-                    flag_equal = mem[mem[pc]] == v;
-                    flag_greater = mem[mem[pc]] > v;
-                    pc += 2;
                 } break;
-            case OpCode::MOVE:
+            case OpCode::NOT:
                 {
-                    int v = mem[mem[pc+1]];
-                    mem[mem[pc]] = v;
-                    pc += 2;
+                } break;
+            case OpCode::LSHIFT:
+                {
+                } break;
+            case OpCode::RSHIFT:
+                {
                 } break;
 
-
-            case OpCode::ADDC:
-                {
-                    int v = mem[pc+1];
-                    mem[mem[pc]] += v;
-                    pc += 2;
-                } break;
-            case OpCode::SUBC:
-                {
-                    int v = mem[pc+1];
-                    mem[mem[pc]] -= v;
-                    pc += 2;
-                } break;
-            case OpCode::MULTC:
-                {
-                    int v = mem[pc+1];
-                    mem[mem[pc]] *= v;
-                    pc += 2;
-                } break;
-            case OpCode::DIVC:
-                {
-                    int v = mem[pc+1];
-                    mem[mem[pc]] /= v;
-                    pc += 2;
-                } break;
-            case OpCode::ANDC:
-                {
-                    int v = mem[pc+1];
-                    mem[mem[pc]] &= v;
-                    pc += 2;
-                } break;
-            case OpCode::ORC:
-                {
-                    int v = mem[pc+1];
-                    mem[mem[pc]] |= v;
-                    pc += 2;
-                } break;
-            case OpCode::XORC:
-                {
-                    int v = mem[pc+1];
-                    mem[mem[pc]] ^= v;
-                    pc += 2;
-                } break;
-            case OpCode::CMPC:
-                {
-                    int v = mem[pc+1];
-                    flag_equal = mem[mem[pc]] == v;
-                    flag_greater = mem[mem[pc]] > v;
-                    pc += 2;
-                } break;
-            case OpCode::MOVEC:
-                {
-                    int v = mem[pc+1];
-                    mem[mem[pc]] = v;
-                    pc += 2;
-                } break;
-            case OpCode::MOVER:
-                {
-                    int v = mem[mem[pc+1]];
-                    int a = mem[mem[pc]];
-                    if(a < code.size)
-                        mem[a] = v;
-                    else if((a-=code.size) < heap.size())
-                        heap[a] = v;
-                    else
-                    {
-                        report_runtime_errro("Tried to access invalid memory");
-                        return;
-                    }
-                    pc += 2;
-                } break;
-            case OpCode::MOVERC:
-                {
-                    int v = mem[pc+1];
-                    int a = mem[mem[pc]];
-                    if(a < code.size)
-                        mem[a] = v;
-                    else if((a-=code.size) < heap.size())
-                        heap[a] = v;
-                    else
-                    {
-                        report_runtime_errro("Tried to access invalid memory");
-                        return;
-                    }
-                    pc += 2;
-                } break;
 
 
             case OpCode::REF:
                 {
-                    int v = mem[pc+1];
-                    mem[mem[pc]] = v;
-                    pc += 2;
                 } break;
             case OpCode::DERF:
                 {
-                    int v = mem[mem[pc+1]];
-                    if(v < code.size)
-                        mem[mem[pc]] = mem[v];
-                    else if((v-=code.size) < heap.size())
-                        mem[mem[pc]] = heap[v];
-                    else
-                    {
-                        report_runtime_errro("Tried to access invalid memory");
-                        return;
-                    }
-                    pc += 2;
+                } break;
+            case OpCode::MOVE:
+                {
+                } break;
+            case OpCode::MOVED:
+                {
+                } break;
+            case OpCode::ALLOC:
+                {
                 } break;
 
-
-            case OpCode::NOT:
-                {
-                    mem[mem[pc]] = ~mem[mem[pc]];
-                    pc += 1;
-                } break;
-            case OpCode::LSHIFT:
-                {
-                    mem[mem[pc++]] <<= 1;
-                } break;
-            case OpCode::RSHIFT:
-                {
-                    mem[mem[pc++]] >>= 1;
-                } break;
 
             case OpCode::GOTO:
                 {
-                    pc = mem[pc+1];
                 } break;
 
-            case OpCode::IFGOTO:
+            case OpCode::IF:
                 {
-                    pc += 2;
-                    switch(mem[pc-2])
-                    {
-                        case EQUAL:  if(flag_equal) pc = mem[pc-1]; break;
-                        case NEQUAL: if(!flag_equal) pc = mem[pc-1]; break;
-                        case LEQUAL: if(!flag_greater) pc = mem[pc-1]; break;
-                        case GEQUAL: if(flag_equal || flag_greater) pc = mem[pc-1]; break;
-                        case LESS:   if(!flag_equal && !flag_greater) pc = mem[pc-1]; break;
-                        case GREAT:  if(flag_greater) pc = mem[pc-1]; break;
-                    }
                 } break;
-
-            case OpCode::ALLOC:
-                {
-                    heap.push_back(0);
-                    heap.push_back(0);
-                    mem[mem[pc]] = heap.size()-2 + code.size;
-                    pc += 1;
-                } break;
-
             case OpCode::PRINT:
                 {
-                    printf("%d", mem[mem[pc++]]);
                 } break;
             case OpCode::PRINTC:
                 {
-                    printf("%c", (char)mem[mem[pc++]]);
                 } break;
         }
     }
