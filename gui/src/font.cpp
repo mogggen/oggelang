067a71640957b7250f1568c10656da37ceebb63b
Magnus Lind <maglii-9-local@B4711.ltuad.ltu.se>
2020-11-12 18:44:06 +0100

    Full speed ahead with fonts

--- gui/src/font.cpp
+++ gui/src/font.cpp
@@ -4,15 +4,29 @@
 #include FT_FREETYPE_H
 
 FT_Library library;
+SDL_Color colors_white[256];
 
-void render_text(SDL_Renderer* renderer)
+bool init_fonts()
 {
-
     if(FT_Init_FreeType(&library))
     {
         printf("Failed to init freetype.\n");
-        return;
+        return false;
     }
+    
+    for(int i=0; i<256; i++)
+    {
+        colors_white[i].r = i;
+        colors_white[i].g = i;
+        colors_white[i].b = i;
+        colors_white[i].a = i;
+    }
+
+    return true;
+}
+
+bool create_font(Font* font, SDL_Renderer* renderer, const char* filename)
+{
 
     FT_Face face;
 
@@ -23,44 +37,98 @@ void render_text(SDL_Renderer* renderer)
     if(error)
     {
         printf("Failed to load font.\n");
-        return;
+        return false;
     }
 
     error = FT_Set_Char_Size( face, 0, 16*64, 300, 300 );
-    error = FT_Set_Pixel_Sizes( face, 0, 16 );
+    error = FT_Set_Pixel_Sizes( face, 0, 32 );
 
-    int A_index = FT_Get_Char_Index(face, 'A');
+    int loaded_chars = 0;
 
-    error = FT_Load_Glyph( face, A_index, 0 );
-
-    error = FT_Render_Glyph( face->glyph, FT_RENDER_MODE_NORMAL );
-
-    int width = face->glyph->bitmap.width;
-    int rows = face->glyph->bitmap.rows;
-
-    printf("wr: %d %d\n", width, rows);
-
-    SDL_Texture* texture;
-    SDL_Surface* surface = SDL_CreateRGBSurfaceFrom(face->glyph->bitmap.buffer,
-                        width, rows,
-                        8,
-                        width,
-                        0, 0, 0, 0x000000ff);
-                        //SDL_PIXELFORMAT_RGB332);
-
-    if(surface == nullptr)
+    for(int i=0; i <= 255; i++)
     {
-        printf("Failed create surface. %s\n", SDL_GetError());
-        return;
+        char c = (char)i;
+        Character* character = font->characters+i;
+    
+        int char_index = FT_Get_Char_Index(face, c);
+        if(char_index == 0)
+            continue;
+
+        error = FT_Load_Glyph( face, char_index, 0 );
+        if(error != 0)
+            continue;
+
+        error = FT_Render_Glyph( face->glyph, FT_RENDER_MODE_NORMAL );
+        if(error != 0)
+            continue;
+
+        int width = face->glyph->bitmap.width;
+        int rows = face->glyph->bitmap.rows;
+
+        if(width != 0 && rows != 0)
+        {
+            SDL_Texture* texture;
+            SDL_Surface* surface = SDL_CreateRGBSurfaceFrom(face->glyph->bitmap.buffer,
+                    width, rows,
+                    8,
+                    face->glyph->bitmap.pitch,
+                    0x00000000, 0x00000000, 0x00000000, 0x000000ff);
+            //format);
+
+
+
+            SDL_SetPaletteColors(surface->format->palette, colors_white, 0, 256);
+
+            if(surface == nullptr)
+            {
+                printf("Failed create surface. %s\n", SDL_GetError());
+                continue;
+            }
+
+            texture = SDL_CreateTextureFromSurface(renderer, surface);
+            if(texture == nullptr)
+            {
+                printf("Failed create texture. %s\n", SDL_GetError());
+                continue;
+            }
+
+            SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+            character->texture = texture;
+        }
+
+        character->is_valid = true;
+        character->width = width;
+        character->height = rows;
+        character->advance = face->glyph->advance.x/64;
+
+        loaded_chars++;
     }
 
-    texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
-    SDL_Rect texture_rect;
-    texture_rect.x = 0;
-    texture_rect.y = 0;
-    texture_rect.w = 400;
-    texture_rect.h = 400;
-    
-    SDL_RenderCopy(renderer, texture, nullptr, &texture_rect);
+    printf("Loaded %d chars\n", loaded_chars);
+    return true;
+}
+
+void render_text(SDL_Renderer* renderer, Font* font, const char* text)
+{
+    int x_offset = 0;
+    while(*text != '\0')
+    {
+        Character* character = font->characters+*text;
+        if(!character->is_valid)
+            continue;
+
+        if(character->texture != nullptr)
+        {
+            SDL_Rect texture_rect;
+            texture_rect.x = x_offset;
+            texture_rect.y = 0;
+            texture_rect.w = character->width;
+            texture_rect.h = character->height;
+
+            SDL_RenderCopy(renderer, character->texture, nullptr, &texture_rect);
+        }
+
+        x_offset += character->advance;
+        text++;
+    }
 }
--- gui/src/font.h
+++ gui/src/font.h
@@ -1,7 +1,25 @@
 #ifndef FONT_H
 #define FONT_H
+
+#include <unordered_map>
 #include "SDL.h"
 
-void render_text(SDL_Renderer* renderer);
+struct Character
+{
+    bool is_valid = false;
+    SDL_Texture* texture = nullptr;
+    int width, height;
+    int advance;
+};
+
+struct Font
+{
+    const char* name;
+    Character characters[256];
+};
+
+bool init_fonts();
+bool create_font(Font* font, SDL_Renderer* renderer, const char* filename);
+void render_text(SDL_Renderer* renderer, Font* font, const char* text);
 
 #endif
